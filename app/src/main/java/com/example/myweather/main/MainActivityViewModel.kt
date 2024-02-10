f152028f308e7962920a99c4bb29a74bc51eaaed
Boralgan <boralgan4444@gmail.com>
2024-02-10 16:44:19 +0300

    -UseCase added.

--- app/src/main/java/com/example/myweather/main/MainActivityViewModel.kt
+++ app/src/main/java/com/example/myweather/main/MainActivityViewModel.kt
@@ -3,46 +3,47 @@ package com.example.myweather.main
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.myweather.data.WeatherRepository
+import com.example.myweather.data.remote.domain.WeatherUseCase
 import com.example.myweather.data.remote.model.WeatherResponseModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
 class MainActivityViewModel @Inject constructor(
-    private val repository: WeatherRepository
+    //  private val repository: WeatherRepository,
+    private val useCase: WeatherUseCase
+
 ) : ViewModel() {
 
     val liveData = MutableLiveData<WeatherResponseModel>()
 
-     fun fetchWeatherData(query: String) {
+    fun fetchWeatherData(query: String) {
 
-        repository.getWeather(query)
-            .flowOn(Dispatchers.IO)
-            .catch {
-                println("DEFAULT ERROR" + it.message)
-            }
-            .onEach {
+        useCase.fetchWeatherData(
+            query,
+            onSuccess = {
                 liveData.value = it
+            },
+            onFailure = {
+                println("HATA $it")
             }
+        )
             .launchIn(viewModelScope)
     }
 
     fun fetchWeatherWithCordData(lat: Double, lon: Double) {
 
-        repository.getWeatherWithCord(lat, lon)
-            .flowOn(Dispatchers.IO)
-            .catch {
-                println("CORD ERROR" + it.message)
-            }
-            .onEach {
+        useCase.fetchWeatherWithCordData(
+            lat,
+            lon,
+            onSuccess = {
                 liveData.value = it
+            },
+            onFailure = {
+                println("HATA $it")
             }
+        )
             .launchIn(viewModelScope)
     }
 }

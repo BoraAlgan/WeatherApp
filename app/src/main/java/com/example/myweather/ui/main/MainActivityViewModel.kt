0472e81cbb1113d91deb2e2cfefcb53f7151c905
Boralgan <boralgan4444@gmail.com>
2024-02-13 20:34:08 +0300

    -NavigationBar added.

--- app/src/main/java/com/example/myweather/ui/main/MainActivityViewModel.kt
+++ app/src/main/java/com/example/myweather/ui/main/MainActivityViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.myweather.main
+package com.example.myweather.ui.main
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
---
+++ app/src/main/java/com/example/myweather/ui/wind/WindViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.myweather.ui.wind
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class WindViewModel @Inject constructor() : ViewModel() {
+
+}
